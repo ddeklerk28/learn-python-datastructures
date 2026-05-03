c4956a3ee3c3beaf454f058c82e45965e2052de2
Dirk de Klerk <d.deklerk1212@gmail.com>
2026-05-03 08:07:47 +0200

    Chore: added setters to Node properties

--- datastructures/singly_linked_list/node.py
+++ datastructures/singly_linked_list/node.py
@@ -7,6 +7,14 @@ class Node:
     def nxt(self):
         return self._nxt
 
+    @nxt.setter
+    def nxt(self, other: Node | None):
+        self._nxt = other
+
     @property
     def value(self):
         return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = new_value
